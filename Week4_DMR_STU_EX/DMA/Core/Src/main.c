2c2b8e0cc708719635cad3743bbaba6cd6706862
Athimet Aiewcharoen <71125762+AthimetA@users.noreply.github.com>
2022-03-08 10:09:20 +0700

    1

--- Week4_DMR_STU_EX/DMA/Core/Src/main.c
+++ Week4_DMR_STU_EX/DMA/Core/Src/main.c
@@ -212,7 +212,7 @@ static void MX_ADC1_Init(void)
   */
   sConfig.Channel = ADC_CHANNEL_1;
   sConfig.Rank = 2;
-  sConfig.SamplingTime = ADC_SAMPLETIME_15CYCLES;
+  sConfig.SamplingTime = ADC_SAMPLETIME_480CYCLES;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
